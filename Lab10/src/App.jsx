200ed1df9b463fc236c40d7945f2b94735c6c323
Roberth <robcorderoa@edu.uc.ac.cr>
2025-12-14 19:30:23 -0600

    lab10

--- Lab10/src/App.jsx
+++ Lab10/src/App.jsx
@@ -1,41 +1,45 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import boy from './assets/boy.png'
 import adult from './assets/adult.png'
 import './App.css'
-import LikeButton from './likeButton.jsx'
+import LikeButton from './LikeButton'
 
 function App() {
-  // useState crea una variable de estado (count) y una función para actualizarla (setCount)
-  const [count, setCount] = useState(0)
-
+  const [age, setAge] = useState(0)
+  const [isAdult, setIsAdult] = useState(false)
+  
+  useEffect(() =>{
+    setIsAdult(age>18)
+  },[age])
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank" rel="noopener noreferrer">
+        <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
+        <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount(count + 1)}>
-          Has hecho clic {count} veces
+        <button onClick={() => setAge((age) => age + 1)}>
+          age is {age}
         </button>
-        <LikeButton />
+
+        <LikeButton></LikeButton>
+
         <p>
-          Edita <code>src/App.jsx</code> y guarda para probar HMR
+          Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
-        Haz clic en los logos de Vite y React para aprender más
+        Click on the Vite and React logos to learn more
       </p>
-      <div>
-        <img src={boy} alt="Boy" />
-        <img src={adult} alt="Adult" />
+      <div className='avatars'>
+        <img src={isAdult ? adult:boy} alt="Avatar" />
       </div>
     </>
   )
--- Lab10/vite.config.js
+++ Lab10/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'II-51-Laboratorios/LAB10/dist',
   plugins: [react()],
 })
